5fcafa49b302b12e9602dcf1f2016af7c90838ea
Érico Andrei <ericof@gmail.com>
2023-11-24 17:54:06 -0300

    Increase test coverage (#39)
    * Add .github/CODEOWNERS
    
    * Remove locales from coverage report
    
    * Increase test coverage of the package

--- tests/controlpanel/conftest.py
+++ tests/controlpanel/conftest.py
@@ -1,13 +1,4 @@
-from plone.app.testing import SITE_OWNER_NAME
-from plone.app.testing import SITE_OWNER_PASSWORD
-from plone.restapi.testing import RelativeSession
-
 import pytest
-
-
-@pytest.fixture()
-def app(functional):
-    return functional["app"]
 
 
 @pytest.fixture()
@@ -19,27 +10,3 @@ def portal(functional):
 @pytest.fixture()
 def http_request(functional):
     return functional["request"]
-
-
-@pytest.fixture()
-def request_factory(portal):
-    def factory():
-        url = portal.absolute_url()
-        api_session = RelativeSession(url)
-        api_session.headers.update({"Accept": "application/json"})
-        return api_session
-
-    return factory
-
-
-@pytest.fixture()
-def anon_request(request_factory):
-    return request_factory()
-
-
-@pytest.fixture()
-def manager_request(request_factory):
-    request = request_factory()
-    request.auth = (SITE_OWNER_NAME, SITE_OWNER_PASSWORD)
-    yield request
-    request.auth = ()
