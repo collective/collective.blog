bed525582fab6b1a9919604b200c6de8b13ce3fe
jonaspiterek <piterek@kitconcept.com>
2023-11-27 12:50:50 +0100

    Add news

--- src/collective/blog/subscribers/auto_add_authors.py
+++ src/collective/blog/subscribers/auto_add_authors.py
@@ -1,5 +1,7 @@
 from plone import api
 
+import uuid
+
 
 def auto_add_authors_container(obj, event):
     authors = api.content.create(
@@ -7,6 +9,10 @@ def auto_add_authors_container(obj, event):
         title="Authors",
         container=obj,
     )
+
+    uuids = [str(uuid.uuid4()), str(uuid.uuid4())]
+    authors.blocks = {uuids[0]: {"@type": "title"}, uuids[1]: {"@type": "slate"}}
+    authors.blocks_layout = {"items": [uuids[0], uuids[1]]}
 
     permission_id = "collective.blog: Add Author"
     roles = ["Manager", "Site Administrator", "Owner", "Editor", "Contributor"]
