5e5dbbaea36ef414d388cd1e23ff427f7c589636
Jonas Piterek <110389276+jonaspiterek@users.noreply.github.com>
2023-12-04 17:01:46 +0100

    Add translation for Authors folder (#51)
    * Add translation for Authors folder
    
    * Fix language detection support
    
    * Tests: Always compile .mo files
    
    ---------
    
    Co-authored-by: Érico Andrei <ericof@gmail.com>

--- src/collective/blog/subscribers/blog.py
+++ src/collective/blog/subscribers/blog.py
@@ -1,20 +1,44 @@
+from collective.blog import _
 from collective.blog import logger
+from collective.blog import PACKAGE_NAME
 from collective.blog.content.blog import Blog
 from plone import api
+from zope.i18nmessageid.message import Message
 
 import uuid
 
+
+_AUTHORS_FOLDERISH = {
+    "id": _("authors_folder_id", default="authors"),
+    "title": _("authors_folder_title", default="Authors"),
+    "description": _("authors_folder_desc", default="Our Authors"),
+}
 
 PERMISSIONS_BLOG = (
     ("collective.blog: Add Blog", []),
     ("collective.blog: Add Author", []),
 )
+
 PERMISSIONS_AUTHORS = (
     (
         "collective.blog: Add Author",
         ["Manager", "Site Administrator", "Owner", "Editor", "Contributor"],
     ),
 )
+
+
+def _translate(msg: Message, lang: str) -> str:
+    """Translate the msg to the desired language.
+
+    This is here due to a bug on api.portal.translate that expects the language code to be
+    on a specific format.
+    ref: https://github.com/plone/plone.api/issues/524
+    """
+    if "-" in lang:
+        lang_ = lang[:2]
+        cc = lang[-2:].upper()
+        lang = f"{lang_}_{cc}"
+    return api.portal.translate(msg, domain=PACKAGE_NAME, lang=lang)
 
 
 def _log_permission_change(path: str, permission_id: str, roles: list):
@@ -32,11 +56,13 @@ def auto_add_authors_container(blog: Blog, event):
     for permission_id, roles in PERMISSIONS_BLOG:
         blog.manage_permission(permission_id, roles=roles, acquire=False)
         _log_permission_change(blog_path, permission_id, roles)
+    lang = blog.language
     # Create Authors container
     authors = api.content.create(
         type="Document",
-        id="authors",
-        title="Authors",
+        id=_translate(_AUTHORS_FOLDERISH["id"], lang=lang),
+        title=_translate(_AUTHORS_FOLDERISH["title"], lang=lang),
+        description=_translate(_AUTHORS_FOLDERISH["description"], lang=lang),
         container=blog,
     )
     # Add Blocks
@@ -45,7 +71,7 @@ def auto_add_authors_container(blog: Blog, event):
     authors.blocks_layout = {"items": [uuids[0], uuids[1]]}
 
     authors_path = "/".join(authors.getPhysicalPath())
-    logger.info(f"Created authors folder inside {blog_path}")
+    logger.info(f"Created {authors.id} folder inside {blog_path}")
     for permission_id, roles in PERMISSIONS_AUTHORS:
         authors.manage_permission(permission_id, roles=roles, acquire=False)
         _log_permission_change(authors_path, permission_id, roles)
