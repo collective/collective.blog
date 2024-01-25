5126df5e52faeddf0c8e55fe5f7dfc57245c44d5
jonaspiterek <piterek@kitconcept.com>
2024-01-25 13:28:55 +0100

    add indexer to index authors from a blog post

--- src/collective/blog/indexers/post.py
+++ src/collective/blog/indexers/post.py
@@ -1,5 +1,6 @@
 from collective.blog.content.post import IPost
 from collective.blog.content.post import Post
+from plone import api
 from plone.indexer import indexer
 
 
@@ -7,3 +8,19 @@ from plone.indexer import indexer
 def blog_post_indexer(obj: Post):
     """Return blog uuid."""
     return obj.blog_uid()
+
+
+@indexer(IPost)
+def post_authors(obj: Post):
+    """Returns the authors of a blog post."""
+    authors = []
+    uids = obj.creators
+    for uid in uids:
+        searchResult = api.content.find(portal_type="Author", UID=uid)
+        if len(searchResult) > 0:
+            author = searchResult[0].getObject()
+
+            if author is not None:
+                authors.append(author.title)
+
+    return authors
