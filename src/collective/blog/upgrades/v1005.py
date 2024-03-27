651a34c594cca6d8fbd6ef29886ddeeea8df84e8
David Glick <david@glicksoftware.com>
2024-03-26 21:44:15 -0700

    add criterion in upgrade but don't overwrite settings

--- src/collective/blog/upgrades/v1005.py
+++
@@ -1,9 +0,0 @@
-from collective.blog.interfaces import IBlogSettings
-from plone.registry.interfaces import IRegistry
-from zope.component import getUtility
-
-
-def update_registry_records(self):
-    # add new persistent setting in registry
-    registry = getUtility(IRegistry)
-    registry.registerInterface(IBlogSettings)
