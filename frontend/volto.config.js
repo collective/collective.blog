06790a59b748d112bf568d8f66daed269af4b882
Jonas Piterek <piterek@kitconcept.com>
2025-05-14 13:55:15 +0200

    chore: add vlt as theme, filterBlobs=true for core

--- frontend/volto.config.js
+++ frontend/volto.config.js
@@ -1,5 +1,8 @@
-const addons = ['@plone-collective/volto-blog'];
-const theme = '';
+const addons = [
+  '@plone-collective/volto-blog',
+  '@kitconcept/volto-light-theme',
+];
+const theme = '@kitconcept/volto-light-theme';
 
 module.exports = {
   addons,
