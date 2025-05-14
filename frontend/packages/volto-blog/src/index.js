82a32065bd10928ed22079598f13a05e09ff002d
Jonas Piterek <piterek@kitconcept.com>
2025-05-14 14:00:11 +0200

    fix: remove import

--- frontend/packages/volto-blog/src/index.js
+++ frontend/packages/volto-blog/src/index.js
@@ -1,12 +1,9 @@
 import PostView from './components/PostView';
-import reducers from './reducers';
 
 const applyConfig = (config) => {
   config.settings.isMultilingual = false;
   config.settings.supportedLanguages = ['en'];
   config.settings.defaultLanguage = 'en';
-
-  config.addonReducers = { ...config.addonReducers, ...reducers };
 
   config.views.contentTypesViews.Post = PostView;
 
