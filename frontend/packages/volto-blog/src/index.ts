99b3aed0d69e58ec527a12a0f0a4a35cc41cf255
Jonas Piterek <piterek@kitconcept.com>
2025-05-19 10:06:19 +0200

    chore: don't set language specific settings

--- frontend/packages/volto-blog/src/index.ts
+++ frontend/packages/volto-blog/src/index.ts
@@ -7,10 +7,6 @@ import installBlocks from './config/blocks';
 const applyConfig = (config: ConfigType) => {
   installBlocks(config);
 
-  config.settings.isMultilingual = false;
-  config.settings.supportedLanguages = ['en'];
-  config.settings.defaultLanguage = 'en';
-
   config.views.contentTypesViews.Post = PostView;
 
   return config;
